0edbb058547a8265853fb5294481915aa19175d7
vmadae <71826351+vmadae@users.noreply.github.com>
2022-05-23 00:37:16 -0400

    shiny

--- server.R
+++ server.R
@@ -1,14 +1,39 @@
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {  
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-  output$distPlot <- renderPlot({
-    x    <- faithful[, 2]  # Old Faithful Geyser data
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-  })
+suppressWarnings(library(shiny))
+
+shinyServer(function(input,output){
+  output$DataScenarios <- DT::renderDataTable(
+    DT::datatable({
+      fileData <- read.delim(file = "./FileSystem/Scenario.txt", header = TRUE, sep = ",", dec = ".")
+    })
+  )
+  output$DataInstances <- DT::renderDataTable(
+    DT::datatable({
+      fileData <- read.delim(file = "./FileSystem/Instances.txt", header = TRUE, sep = ",", dec = ".")
+    })
+  )
+  output$DataParameters <- DT::renderDataTable(
+    DT::datatable({
+      fileData <- read.delim(file = "./FileSystem/Parameters.txt", header = TRUE, sep = ",", dec = ".")
+    })
+  )
+  output$DataTarget <- DT::renderDataTable(
+    DT::datatable({
+      fileData <- read.delim(file = "./FileSystem/Target.txt", header = TRUE, sep = ",", dec = ".")
+    })
+  )
+  output$DataExperiment <- DT::renderDataTable(
+    DT::datatable({
+      fileData <- read.delim(file = "./FileSystem/Experiment.txt", header = TRUE, sep = ",", dec = ".")
+    })
+  )
+  output$DataIterations <- DT::renderDataTable(
+    DT::datatable({
+      fileData <- read.delim(file = "./FileSystem/Iteration.txt", header = TRUE, sep = ",", dec = ".")
+    })
+  )
+  output$DataVersion <- DT::renderDataTable(
+    DT::datatable({
+      fileData <- read.delim(file = "./FileSystem/Version.txt", header = TRUE, sep = ",", dec = ".")
+    })
+  )
 })
--- ui.R
+++ ui.R
@@ -1,18 +1,43 @@
-shinyUI(fluidPage){
-  # Application title
-  titlePanel("Benchmark management system for irace")
-  # Sidebar with a slider input for the number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
-}
+suppressWarnings(library(shiny))
+
+shinyUI(fluidPage(
+  #Aplication title
+  titlePanel("Benchmark management system for irace"),
+  
+  fluidPage(tabsetPanel(
+    tabPanel("Scenario", 
+             titlePanel("Scenario"),
+             hr(),
+             DT::dataTableOutput("DataScenarios", width = 12)
+            ),
+    tabPanel("Instances",
+             titlePanel("Instances"),
+             hr(),
+             DT::dataTableOutput("DataInstances", width = 5)
+            ),
+    tabPanel("Parameters", 
+             titlePanel("Parameters"),
+             hr(),
+             DT::dataTableOutput("DataParameters", width = 5)
+            ),
+    tabPanel("Target", 
+             titlePanel("Target"),
+             hr(),
+             DT::dataTableOutput("DataTarget", width = 5)
+            ),
+    tabPanel("Experiments", 
+             titlePanel("Experiments"),
+             hr(),
+             DT::dataTableOutput("DataExperiment", width = 5)
+            ),
+    tabPanel("Iterations",
+             titlePanel("Iterations"),
+             hr(),
+             DT::dataTableOutput("DataIterations", width = 5)
+            ),
+    tabPanel("Version", 
+             titlePanel("Versions"),
+             hr(),
+             DT::dataTableOutput("DataVersion", width = 5)
+             )))
+))
